2958725e62651fd2c9392ee0994fd84483377cb4
Chrisi Webster <christinawebster.cw@gmail.com>
2021-06-27 07:15:15 +0100

    changing so this is just a template

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,35 +1,11 @@
-import React, { useState } from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
-
-//Components
-import Header from "../Header";
-import NavBar from "../NavBar";
-import Home from "../Home";
-import SearchWrapper from "../SearchWrapper";
+import React from "react";
 
 import "./App.css";
 
 //App component
 const App = () => {
-  const [status, setStatus] = useState(false);
-
-  const handleNavClick = () => {
-    status === true ? setStatus(false) : setStatus(true);
-  };
-
-  return (
-    <div>
-      <Header title="Playlist Maker">
-        <NavBar handleNavClick={handleNavClick} status={status} />
-      </Header>
-      <HashRouter basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/search" component={SearchWrapper} />
-        </Switch>
-      </HashRouter>
-    </div>
-  );
+  return;
+  <h1>Webpack template</h1>;
 };
 
 export default App;
